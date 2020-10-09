9115c94ad6fe216a073047aa988fae321d8bd3a1
Fabian Peña <f.pena.jacobo@gmail.com>
2020-10-08 20:23:05 -0500

    ADD: new branch

--- handler.js
+++ handler.js
@@ -1,12 +1,32 @@
 'use strict';
+const { Octokit } = require("@octokit/rest");
 
 module.exports.getCommits = async event => {
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN
+  });
+  const repoNames = ['github-test', 'angular-test'];
+  const owner = 'fabian818';
+  const repos = repoNames.map(repoName => {
+    return {
+      name: repoName,
+      commits: (await octokit.repos.listCommits({
+        owner,
+        repoName,
+      })).data.map(obj => {
+        return {
+          message: obj.commit.message,
+          date: obj.commit.committer.date
+        };
+      })
+    } 
+  });
+  console.log(commits);
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        repos: repos
       },
       null,
       2
